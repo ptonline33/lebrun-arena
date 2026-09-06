a6c2ccc8ca86447a9a7794356c220a7ea54ea3f0
Pradeep Talwar <ptonline40@gmail.com>
2026-09-05 21:44:47 -0400

    Allow landscape orientation in PWA so video fullscreen can rotate

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,7 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     scope: "/",
     display: "standalone",
-    orientation: "portrait",
+    orientation: "any",
     background_color: "#04050d",
     theme_color: "#04050d",
     categories: ["sports", "entertainment"],
